dd8bce6f620af35b1529762daa76daaf3fb35dac
Atamert Ölçgen <muhuk@muhuk.com>
2010-02-25 21:31:21 +0200

    Import PERFORMANCE_FUNC via function

--- invitation/app_settings.py
+++ invitation/app_settings.py
@@ -3,18 +3,24 @@ from django.core.exceptions import ImproperlyConfigured
 from django.utils.importlib import import_module
 
 
+def get_performance_func(settings):
+    performance_func = getattr(settings, 'INVITATION_PERFORMANCE_FUNC', None)
+    if isinstance(performance_func, (str, unicode)):
+        module_name, func_name = performance_func.rsplit('.', 1)
+        try:
+            performance_func = getattr(import_module(module_name), func_name)
+        except ImportError:
+            raise ImproperlyConfigured('Can\'t import performance function ' \
+                                       '`%s` from `%s`' % (func_name,
+                                                           module_name))
+    if performance_func and not callable(performance_func):
+        raise ImproperlyConfigured('INVITATION_PERFORMANCE_FUNC must be a ' \
+                                   'callable or an import path string ' \
+                                   'pointing to a callable.')
+
+
 INVITE_ONLY = getattr(settings, 'INVITATION_INVITE_ONLY', False)
 EXPIRE_DAYS = getattr(settings, 'INVITATION_EXPIRE_DAYS', 15)
 INITIAL_INVITATIONS = getattr(settings, 'INVITATION_INITIAL_INVITATIONS', 10)
 REWARD_THRESHOLD = getattr(settings, 'INVITATION_REWARD_THRESHOLD', 0.75)
-PERFORMANCE_FUNC = getattr(settings,
-                           'INVITATION_PERFORMANCE_FUNC',
-                           None)
-if isinstance(PERFORMANCE_FUNC, (str, unicode)):
-    module_name, func_name = PERFORMANCE_FUNC.rsplit('.', 1)
-    print dir(import_module(module_name))
-    PERFORMANCE_FUNC = getattr(import_module(module_name), func_name)
-if PERFORMANCE_FUNC and not callable(PERFORMANCE_FUNC):
-    raise ImproperlyConfigured('INVITATION_PERFORMANCE_FUNC must be a ' \
-                               'callable or an import path string pointing ' \
-                               'to a callable.')
+PERFORMANCE_FUNC = get_performance_func(settings)
