1ac79c225602f4dc9dba725dea99deb43449d6f0
Atamert Ölçgen <muhuk@muhuk.com>
2010-02-28 21:08:11 +0200

    setup.py & packaging stuff

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from invitation import __version__, __maintainer__, __email__
+
+
+setup(
+    name = 'django-invitation',
+    version = __version__,
+    url = 'http://github.com/muhuk/django-invitation',
+    author = __maintainer__.encode('utf8'),
+    author_email = __email__,
+    license = open('LICENSE.txt').read(),
+    packages = ['invitation',
+                'invitation.templatetags'],
+    package_data= {'invitation': ['templates/*',
+                                  'locale/*/LC_MESSAGES/django.po']},
+    data_files=[('', ['LICENSE.txt',
+                      'README.rst'])],
+    description = 'Registration through invitations',
+    long_description=open('README.rst').read(),
+    classifiers = ['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content']
+)
