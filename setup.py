afbd4b582f122c331514a0ae1df37f3f705b0989
Atamert Ölçgen <muhuk@muhuk.com>
2010-03-27 13:21:52 +0200

    Added tests and translations to packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
+import os
 from distutils.core import setup
+from django.core.management.commands.compilemessages import compile_messages
 from invitation import __version__, __maintainer__, __email__
+
+
+def compile_translations():
+    curdir = os.getcwdu()
+    os.chdir(os.path.join(os.path.dirname(__file__), 'invitation'))
+    compile_messages()
+    os.chdir(curdir)
+compile_translations()
+
+
+license_text = open('LICENSE.txt').read()
+long_description = open('README.rst').read()
 
 
 setup(
     name = 'django-inviting',
     version = __version__,
     url = 'http://github.com/muhuk/django-inviting',
-    author = __maintainer__.encode('utf8'),
+    author = __maintainer__,
     author_email = __email__,
-    license = open('LICENSE.txt').read(),
+    license = license_text,
     packages = ['invitation',
+                'invitation.tests',
                 'invitation.templatetags'],
     package_data= {'invitation': ['templates/*',
-                                  'locale/*/LC_MESSAGES/django.po']},
+                                  'tests/templates/*'
+                                  'locale/*/LC_MESSAGES/django.*']},
     data_files=[('', ['LICENSE.txt',
                       'README.rst'])],
     description = 'Registration through invitations',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     classifiers = ['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Framework :: Django',
