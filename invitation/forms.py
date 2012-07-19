21b14a946dbec3d23c115b1c0ca0141b7a566c9a
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2012-07-19 22:46:39 +0200

    Replace profile_callback usage with registration.signals.user_registered signal

--- invitation/forms.py
+++ invitation/forms.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import User
 from registration.forms import RegistrationForm
 
 
-def save_user(form_instance, profile_callback=None):
+def save_user(form_instance):
     """
     Create a new **active** user from form data.
 
     This method is intended to replace the ``save`` of
-    ``django-registration``s ``RegistrationForm``. Calls
-    ``profile_callback`` if provided. Required form fields
+    ``django-registration``s ``RegistrationForm``. Required form fields
     are ``username``, ``email`` and ``password1``.
     """
     username = form_instance.cleaned_data['username']
@@ -17,8 +16,6 @@ def save_user(form_instance, profile_callback=None):
     password = form_instance.cleaned_data['password1']
     new_user = User.objects.create_user(username, email, password)
     new_user.save()
-    if profile_callback is not None:
-        profile_callback(user=new_user)
     return new_user
 
 
