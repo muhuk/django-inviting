a64c215dd7f6b1de89fff5e3d5b42a5d51fa03aa
Brad Montgomery <brad@bradmontgomery.net>
2012-08-04 13:36:58 -0500

    updated InvitationStats' post_syncdb handler so it only creates an object for users that didn't already have one

--- invitation/management/__init__.py
+++ invitation/management/__init__.py
@@ -1,17 +1,18 @@
-from sys import stdout
-
 from django.contrib.auth.models import User
 from django.db.models.signals import post_syncdb
 from invitation import models
 
 
-def create_stats_for_all_users(sender, **kwargs):
+def create_stats_for_existing_users(sender, **kwargs):
     """
     Create `InvitationStats` objects for all users after a `sycndb`
 
     """
-    for u in User.objects.all():
+    count = 0
+    for u in User.objects.filter(invitation_stats__isnull=True):
         models.InvitationStats.objects.create(user=u)
-    stdout.write("Created InvitationStats for all existing Users\n")
+        count += 1
+    if count > 0:
+        print "Created InvitationStats for %s existing Users" % count
 
-post_syncdb.connect(create_stats_for_all_users, sender=models)
+post_syncdb.connect(create_stats_for_existing_users, sender=models)
